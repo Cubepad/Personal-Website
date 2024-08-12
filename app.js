63f9ebd12057902523663e80466dcc1106cbfdb7
David Olaniyan <davideniola108@gmail.com>
2024-08-12 16:36:40 +0200

    Improved formatting for all files with prettier

--- app.js
+++ app.js
@@ -1,33 +1,27 @@
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      console.log(entry)
-      if (entry.isIntersecting) {
-      entry.target.classList.add('show');
-
-      }
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
   });
 });
-  
-  
-  const hiddenElements = document.querySelectorAll('.hidden');
-  hiddenElements.forEach((el) => observer.observe(el));
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
 
 let btn = document.getElementById("btn");
 let logo = document.getElementById("logo");
 let logo_enlarged = document.getElementById("logo_enlarged");
 
+btn.onclick = function () {
+  document.body.classList.toggle("light-theme");
 
-  btn.onclick = function(){
-    document.body.classList.toggle("light-theme");
-
-    if(document.body.classList.contains("light-theme")){
-      logo.src ="logo/personal-logo-black-no-detail.svg";
-      logo_enlarged.src ="logo/personal-logo-black-no-detail-outline.svg";
-    }else{
-      logo.src ="logo/personal-logo-white-no-detail.svg";
-      logo_enlarged.src ="logo/personal-logo-white-no-detail-v2.svg";
-    }
+  if (document.body.classList.contains("light-theme")) {
+    logo.src = "logo/personal-logo-black-no-detail.svg";
+    logo_enlarged.src = "logo/personal-logo-black-no-detail-outline.svg";
+  } else {
+    logo.src = "logo/personal-logo-white-no-detail.svg";
+    logo_enlarged.src = "logo/personal-logo-white-no-detail-v2.svg";
   }
-
-
-  
+};

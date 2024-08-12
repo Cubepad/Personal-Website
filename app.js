2df1b9a0b2f857b18d729ff721857c6b292b77ff
David Olaniyan <davideniola108@gmail.com>
2024-08-12 11:24:11 +0200

    Added Light Theme. Changed job title. changed background when on light theme

--- app.js
+++ app.js
@@ -12,4 +12,11 @@ const observer = new IntersectionObserver((entries) => {
   const hiddenElements = document.querySelectorAll('.hidden');
   hiddenElements.forEach((el) => observer.observe(el));
 
+  let btn = document.getElementById("btn");
+
+  btn.onclick = function(){
+    document.body.classList.toggle("light-theme");
+  }
+
+
   

0aa16439d105a0d384aebef693950507a5f316b1
David Olaniyan <davideniola108@gmail.com>
2024-08-12 13:06:01 +0200

    Changed img file types. Switched dynamic images for themes

--- app.js
+++ app.js
@@ -12,10 +12,21 @@ const observer = new IntersectionObserver((entries) => {
   const hiddenElements = document.querySelectorAll('.hidden');
   hiddenElements.forEach((el) => observer.observe(el));
 
-  let btn = document.getElementById("btn");
+let btn = document.getElementById("btn");
+let logo = document.getElementById("logo");
+let logo_enlarged = document.getElementById("logo_enlarged");
+
 
   btn.onclick = function(){
     document.body.classList.toggle("light-theme");
+
+    if(document.body.classList.contains("light-theme")){
+      logo.src ="logo/personal-logo-black-no-detail.svg";
+      logo_enlarged.src ="logo/personal-logo-black-no-detail-outline.svg";
+    }else{
+      logo.src ="logo/personal-logo-white-no-detail.svg";
+      logo_enlarged.src ="logo/personal-logo-white-no-detail-v2.svg";
+    }
   }
 
 

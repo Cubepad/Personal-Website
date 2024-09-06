482b75037f3c01726beeef0f4a906da78fc1acdc
David Olaniyan <davideniola108@gmail.com>
2024-09-06 22:37:23 +0200

    Changed all references of btn to theme_btn. Improved padding of hero section by removing some at the top and adding more to the bottom.

--- javascript/app.js
+++ javascript/app.js
@@ -15,21 +15,21 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
 
-let btn = document.getElementById("btn");
+let theme_btn = document.getElementById("theme_btn");
 let logo = document.getElementById("logo");
 let logo_enlarged = document.getElementById("logo_enlarged");
 
-btn.onclick = function () {
+theme_btn.onclick = function () {
   document.body.classList.toggle("light-theme");
 
   if (document.body.classList.contains("light-theme")) {
     logo.src = "logo/personal-logo-black-no-detail.svg";
     logo_enlarged.src = "logo/personal-logo-black-no-detail-outline.svg";
-    btn.textContent = "light_mode";
+    theme_btn.textContent = "light_mode";
   } else {
     logo.src = "logo/personal-logo-white-no-detail.svg";
     logo_enlarged.src = "logo/personal-logo-white-no-detail-v2.svg";
-    btn.textContent = "dark_mode";
+    theme_btn.textContent = "dark_mode";
   }
 };
 
